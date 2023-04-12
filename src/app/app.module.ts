70c70d87d8d5d88649fd03f5aa437928eee6fead
raywu <ray.wu@brandingbrand.com>
2023-04-12 07:24:51 -0700

    add pip component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PipComponent } from './pip/pip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pip/pip.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pip',
+  templateUrl: './pip.component.html',
+  styleUrls: ['./pip.component.css']
+})
+export class PipComponent {
+
+}
