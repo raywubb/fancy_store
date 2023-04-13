049ac66f105e9fcc9d3b3cf7c73b1d9c4ca3eed3
raywu <ray.wu@brandingbrand.com>
2023-04-13 01:22:27 -0700

    pdp css tune

--- src/app/pdp/pdp.component.ts
+++ src/app/pdp/pdp.component.ts
@@ -15,12 +15,22 @@ export class PdpComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    productService.dataReady.subscribe((value) => {
+      this.product = this.productService.getProduct(this.id);
+    });
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.product = this.productService.getProduct(this.id);
+      console.log('.. L24', this.product);
     });
+  }
+
+  getRate() {
+    const value = Math.round(this.product?.rating?.rate ?? 0);
+    return new Array(value);
   }
 }
